ab1ebf4ae6efbed28ab84e05de6ad9740a32c4bc
maarga1516daw2 <maarga1516daw2@gmail.com>
2017-06-05 19:31:00 +0200

    Some testing on the react-bootstrap module

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,46 @@
 
 import * as React from 'react';
+import {Col, Panel} from 'react-bootstrap';
 
 
 export default class App extends React.Component<undefined, undefined> {
 
     public render(): JSX.Element {
         return <div>
-            <h1>HMR is awesome</h1>
-            <p>Full boilerplate for react and typescript with webpack2 and hot module replacement</p>
+            <Col xs={12} className="text-center">
+                <h1>Reactdoro</h1>
+                <h3>A pomodoro-like application made with react and typescript</h3>
+            </Col>
+
+            <Col xs={12} md={4}>
+
+                <Panel header={"Timer"} bsStyle="primary">
+                    00:00
+                </Panel>
+
+            </Col>
+
+            <Col xs={12} md={4}>
+
+                <Panel header={"Tasks to do"} bsStyle="primary">
+                    <ul>
+                        <li>A</li>
+                        <li>B</li>
+                    </ul>
+                </Panel>
+
+            </Col>
+
+            <Col xs={12} md={4}>
+
+                <Panel header={"Completed Tasks"} bsStyle="primary">
+                    <ul>
+                        <li>A</li>
+                        <li>B</li>
+                    </ul>
+                </Panel>
+
+            </Col>
         </div>;
     }
 
