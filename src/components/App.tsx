507ec1a549cac8feae72c94c2029f5573b7af510
maarga1516daw2 <maarga1516daw2@gmail.com>
2017-06-05 19:02:29 +0200

    added react-bootstrap to the project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ export default class App extends React.Component<undefined, undefined> {
 
     public render(): JSX.Element {
         return <div>
-            <h1>Hello World!</h1>
+            <h1>HMR is awesome</h1>
             <p>Full boilerplate for react and typescript with webpack2 and hot module replacement</p>
         </div>;
     }
