aa71e7b1d4bb2b6b33951927434d98d61e3bff4e
maarga1516daw2 <maarga1516daw2@gmail.com>
2017-06-06 19:58:35 +0200

    Hover implemented to help gerard

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import {Component} from 'react';
 import {Task} from '../model/Task';
+import CSSProperties = React.CSSProperties;
 
 export interface ListItemProps {
     task: Task;
@@ -9,10 +10,31 @@ export interface ListItemProps {
     onMark: any;
 }
 
-export default class ListItem extends Component<ListItemProps, {}> {
+export interface ListItemState {
+    hover: boolean;
+}
+
+export default class ListItem extends Component<ListItemProps, ListItemState> {
+
+    public constructor(props: ListItemProps, context: any) {
+        super(props, context);
+        this.state = {hover: false};
+        this.handleMouseHover = this.handleMouseHover.bind(this);
+    }
+
+    private handleMouseHover(event: any): void {
+        this.setState({hover: !this.state.hover});
+    }
 
     render() {
-        return <li>{this.props.task.name} <button onClick={() => this.props.onDelete(this.props.task.id)}>Delete</button></li>;
+
+        let style: CSSProperties = {
+            color: this.state.hover ? 'red' : 'black'
+        };
+
+        return <li onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover} style={style}>
+            {this.props.task.name} <button onClick={() => this.props.onDelete(this.props.task.id)}>Delete</button>
+        </li>;
     }
 
 }
